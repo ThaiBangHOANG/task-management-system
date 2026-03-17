c16d748df082615894b9e118eae64fa3d16cf3bc
thaibangHOANG <thaibang1983hn@gmail.com>
2026-03-17 19:08:02 +0100

    initial setup

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.API.Models;
+
+namespace TaskManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetTasks()
+        {
+            var tasks = new List<TaskItem>
+            {
+                new TaskItem
+                {
+                    Id = 1,
+                    Title = "Learn .NET Web API",
+                    Description = "Create first API endpoint",
+                    Status = "In Progress"
+                }
+            };
+
+            return Ok(tasks);
+        }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagementSystem.API.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Status { get; set; } = "To Do";
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
